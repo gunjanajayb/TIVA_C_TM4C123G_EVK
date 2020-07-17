ae1ef9acd727169e073fb8566e120156286df457
gunjanajayb <55190498+gunjanajayb@users.noreply.github.com>
2020-07-18 02:21:01 +0530

    Added new files symbols.c & .h
    This code has symbols and logos added to it. (EasEmsys logo)

---
+++ OLED_I2C/symbols.h
@@ -0,0 +1,22 @@
+/*
+ * symbols.h
+ *
+ *  Created on: 18-Jul-2020
+ *      Author: bgunjan
+ */
+
+#include <stdint.h>
+
+#ifndef SYMBOLS_H_
+#define SYMBOLS_H_
+
+extern uint8_t clearDisp[128*8];
+extern uint8_t hello[128*8];
+extern uint8_t logo_main[128*8];
+
+
+extern uint16_t charset[8*26];
+
+
+
+#endif /* SYMBOLS_H_ */
